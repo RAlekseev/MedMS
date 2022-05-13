89557b05959c30e9ac5220676676f515a1f694ae
RAlekseev <roma99@bk.ru>
2022-05-14 00:03:15 +0300

    Sidebar permissions control

--- resources/js/core/components/SideBar.vue
+++ resources/js/core/components/SideBar.vue
@@ -2,98 +2,100 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <router-link class="bg-secondary sidebar-brand d-flex align-items-center justify-content-center" :to="{ name: 'home' }">
+        <router-link class="bg-secondary sidebar-brand d-flex align-items-center justify-content-center"
+                     :to="{ name: 'home' }">
 
             <div class="sidebar-brand-icon">
                 <img src="/storage/light_logo.png" alt="alt" height="50"
                      onError="this.onerror=null;this.src='/assets/img/light_logo.png';"/>
-<!--                <i class="fas fa-heartbeat"></i>-->
+                <!--                <i class="fas fa-heartbeat"></i>-->
             </div>
             <div class="sidebar-brand-text mx-3">
-<!--                MedMS<sup>1</sup>-->
+                <!--                MedMS<sup>1</sup>-->
                 {{config_value('org_name')}}
             </div>
         </router-link>
 
 
-        <!-- Divider -->
-<!--        <hr class="sidebar-divider my-0">-->
+        <!--        <hr class="sidebar-divider my-0">-->
 
-        <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="index.html">
+            <router-link class="nav-link" to="/dashboard" v-if="can('dashboard')">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Панель управления</span></a>
+                <span>Панель управления</span></router-link>
         </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Контроль пользователей
-        </div>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-               aria-expanded="true" aria-controls="collapsePages">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Доступ</span>
-            </a>
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Доступ:</h6>
-                    <router-link class="collapse-item" :to="{name: 'rolesIndex'}">Роли</router-link>
-                    <router-link class="collapse-item" :to="{name: 'permissionsIndex'}">Права доступа</router-link>
-
-                </div>
+        <div v-if="can('permissions-index', 'roles-index', 'users-index')">
+            <hr class="sidebar-divider">
+            <div class="sidebar-heading">
+                Контроль пользователей
             </div>
-        </li>
 
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                   aria-expanded="true" aria-controls="collapsePages">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Доступ</span>
+                </a>
+                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Доступ:</h6>
+                        <router-link v-if="can('roles-index')" class="collapse-item" :to="{name: 'rolesIndex'}">Роли
+                        </router-link>
+                        <router-link v-if="can('permissions-index')" class="collapse-item"
+                                     :to="{name: 'permissionsIndex'}">
+                            Права доступа
+                        </router-link>
 
-        <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'usersIndex' }">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Пользователи</span>
-            </router-link>
-        </li>
+                    </div>
+                </div>
+            </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Услуги
+            <li class="nav-item">
+                <router-link v-if="can('users-index')" class="nav-link" :to="{ name: 'usersIndex' }">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Пользователи</span>
+                </router-link>
+            </li>
         </div>
-        <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'servicesIndex' }">
-                <i class="fas fa-fw fa-medkit"></i>
-                <span>Список услуг</span>
-            </router-link>
-        </li>
 
-        <li class="nav-item">
-            <router-link class="nav-link" to="/categories">
-<!--                <i class="fa-fw fa-duotone fa-layer-group"></i>-->
-                <i class="fas fa-fw fa-layer-group"></i>
-                <span>Категории услуг</span>
-            </router-link>
-        </li>
 
-        <li class="nav-item">
-            <router-link class="nav-link" to="/contracts/create">
-                <i class="fas fa-fw fa-check-square"></i>
-                <span>Корзина клиента</span>
-            </router-link>
-        </li>
+        <div v-if="can('contracts-index', 'contracts-create', 'categories-index', 'services-index')">
+            <hr class="sidebar-divider">
+            <div class="sidebar-heading">
+                Услуги
+            </div>
 
-        <li class="nav-item">
-            <router-link class="nav-link" to="/contracts">
-                <i class="fas fa-fw fa-list"></i>
-                <span>Оформленные услуги</span>
-            </router-link>
-        </li>
+            <li class="nav-item">
+                <router-link v-if="can('services-index')" class="nav-link" :to="{ name: 'servicesIndex' }">
+                    <i class="fas fa-fw fa-medkit"></i>
+                    <span>Список услуг</span>
+                </router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link v-if="can('categories-index')" class="nav-link" to="/categories">
+                    <!--                <i class="fa-fw fa-duotone fa-layer-group"></i>-->
+                    <i class="fas fa-fw fa-layer-group"></i>
+                    <span>Категории услуг</span>
+                </router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link v-if="can('contracts-create')" class="nav-link" to="/contracts/create">
+                    <i class="fas fa-fw fa-check-square"></i>
+                    <span>Корзина клиента</span>
+                </router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link v-if="can('contracts-index')" class="nav-link" to="/contracts">
+                    <i class="fas fa-fw fa-list"></i>
+                    <span>Оформленные услуги</span>
+                </router-link>
+            </li>
+        </div>
 
         <!-- Divider -->
         <hr class="sidebar-divider d-none d-md-block">
@@ -121,65 +123,68 @@
             </router-link>
         </li>
 
-        <hr class="sidebar-divider d-none d-md-block">
+        <div v-if="can('schedules-create')">
+            <hr class="sidebar-divider d-none d-md-block">
+            <div class="sidebar-heading">
+                Сотрудники
+            </div>
 
-        <div class="sidebar-heading">
-            Сотрудники
+            <!--        <li class="nav-item">-->
+            <!--            <router-link class="nav-link" to="/settings">-->
+            <!--                <i class="fas fa-fw fa-users"></i>-->
+            <!--                <span>Список сотрудников</span>-->
+            <!--            </router-link>-->
+            <!--        </li>-->
+            <li v-if="can('schedules-index')" class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'schedulesIndex' }">
+                    <i class="fas fa-fw fa-calendar"></i>
+                    <span>Расписание</span>
+                </router-link>
+            </li>
         </div>
 
-        <li class="nav-item">
-            <router-link class="nav-link" to="/settings">
-                <i class="fas fa-fw fa-users"></i>
-                <span>Список сотрудников</span>
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'schedulesIndex' }">
-                <i class="fas fa-fw fa-calendar"></i>
-                <span>Расписание</span>
-            </router-link>
-        </li>
-
-
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <div class="sidebar-heading">
-            Другое
+        <div v-if="can('settings-index', 'doc-templates-index', 'icons-index')">
+            <hr class="sidebar-divider d-none d-md-block">
+            <div class="sidebar-heading">
+                Другое
+            </div>
+            <li class="nav-item">
+                <router-link v-if="can('settings-index')" class="nav-link" to="/settings">
+                    <i class="fas fa-fw fa-cogs"></i>
+                    <span>Настройки</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link v-if="can('doc-templates-index')" class="nav-link" to="/doc_templates">
+                    <i class="fas fa-fw fa-file"></i>
+                    <span>Шаблоны документов</span>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link v-if="can('icons-index')" class="nav-link" to="/icons">
+                    <i class="fas fa-fw fa-images"></i>
+                    <span>Иконки</span>
+                </router-link>
+            </li>
         </div>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/settings">
-                <i class="fas fa-fw fa-cogs"></i>
-                <span>Настройки</span>
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/doc_templates">
-                <i class="fas fa-fw fa-file"></i>
-                <span>Шаблоны документов</span>
-            </router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/icons">
-                <i class="fas fa-fw fa-images"></i>
-                <span>Иконки</span>
-            </router-link>
-        </li>
 
-        <hr class="sidebar-divider d-none d-md-block">
+        <div v-if="can('warehouse-index', 'warehouse-movements')">
+            <hr class="sidebar-divider d-none d-md-block">
+            <div class="sidebar-heading">
+                Склад
+            </div>
 
-        <div class="sidebar-heading">
-            Склад
+            <li class="nav-item">
+                <router-link v-if="can('warehouse-index')" class="nav-link" to="/warehouse">
+                    <i class="fas fa-fw fa-pills"></i>
+                    <span>Аптечный склад</span>
+                </router-link>
+                <router-link v-if="can('warehouse-movements')" class="nav-link" to="/warehouse/movements">
+                    <i class="fas fa-fw fa-share-square"></i>
+                    <span>Передвижения</span>
+                </router-link>
+            </li>
         </div>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/warehouse">
-                <i class="fas fa-fw fa-pills"></i>
-                <span>Аптечный склад</span>
-            </router-link>
-            <router-link class="nav-link" to="/warehouse/movements">
-                <i class="fas fa-fw fa-share-square"></i>
-                <span>Передвижения</span>
-            </router-link>
-        </li>
 
         <!-- Sidebar Toggler (Sidebar) -->
         <div class="text-center d-none d-md-inline">
@@ -191,9 +196,13 @@
 
 <script>
     import {mapGetters} from "vuex"
+
     export default {
         computed: {
-            ...mapGetters(['config_value'])
+            ...mapGetters([
+                'config_value',
+                'can',
+            ])
         }
     }
 </script>
